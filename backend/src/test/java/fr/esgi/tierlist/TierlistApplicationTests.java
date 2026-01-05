edcaf2ef5faa25ae80d403b739060ae41609c1e9
IPandragonI <mathysf84pro@gmail.com>
2026-01-05 09:52:25 +0100

    Initial commit

---
+++ backend/src/test/java/fr/esgi/tierlist/TierlistApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.tierlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
